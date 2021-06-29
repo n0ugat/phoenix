f98ff4844acb8168a40bf5ad0f2540946d7fd096
n0ugat <74826473+n0ugat@users.noreply.github.com>
2021-06-29 17:34:11 +0200

    Add files via upload

---
+++ NDVI-converter.py
@@ -0,0 +1,31 @@
+from PIL import Image
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from matplotlib.colors import ListedColormap, LinearSegmentedColormap
+
+viridis = cm.get_cmap('viridis')
+im_mask = Image.open(str('C:/Users/Admin/desktop/phoenix/phoenix_mask.jpg')).convert('RGB')
+def within_mask(mask_r, mask_g, mask_b):
+    return mask_r == 255 and mask_g == 255 and mask_b == 255
+
+for PhotoNummber in range(78,79):
+    im = Image.open(str('C:/Users/Admin/desktop/phoenix/phoenix_' + str(PhotoNummber).zfill(3) + '.jpg')).convert('RGB')
+    for x in range(0,2592):
+        if x%10==0:
+            print(str(round(x/2592*100)))
+        for y in range (0, 1944):
+            mask_r, mask_g, mask_b = im_mask.getpixel((x, y))
+            if within_mask(mask_r, mask_g, mask_b):
+                r, g, b = im.getpixel((x, y))
+                if r+b!=0:
+                    NDVI=((r-b)/(r+b)+1)/2
+                else:
+                    NDVI=0
+                NDVI_AMP=(NDVI**7)*48
+                RGB_float=viridis(NDVI_AMP)
+                RGB_int=(round(RGB_float[0]*255), round(RGB_float[1]*255), round(RGB_float[2]*255))
+                im.putpixel((x,y), RGB_int)
+            else:
+                im.putpixel((x,y), (0,0,0,0))
+    im.save('c:/Users/Admin/desktop/NDVI/result' + str(PhotoNummber).zfill(3) + '.png')
